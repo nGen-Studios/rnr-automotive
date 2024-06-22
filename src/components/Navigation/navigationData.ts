5c3978477d85e79453d52543677a6b6c78dca502
AngiQiii <118297196+AngiQiii@users.noreply.github.com>
2024-06-22 20:08:04 +0200

    Footer (#9)
    * made the footer replaced the logo and did the responsiveness
    
    * Made the contact page and did the responsiveness

--- src/components/Navigation/navigationData.ts
+++ src/components/Navigation/navigationData.ts
@@ -2,5 +2,5 @@ export const navigationData = [
   { name: "Home", href: "/" },
   { name: "What we do", href: "#whatwedo" },
   { name: "Services", href: "#services" },
-  { name: "Contact us", href: "/contact-us" },
+  { name: "Contact us", href: "/contact" },
 ];
