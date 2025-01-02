753d1733403be17c4c82af683f144e0bf6ccac3a
Wickus Van der Merwe <wickus@comcorp.co.za>
2025-01-02 13:44:30 +0200

    fixed typo in className

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import NavigationLogo from "./NavigationLogo";
 
 export default function Navigation() {
   return (
-    <nav className="w-full bg-primary h-20 flex items-center justify-between absolute top-0 left-0 z-10 text-black">
+    <nav className="w-full bg-primary/[0.4] h-20 flex items-center justify-between absolute top-0 left-0 z-10 text-black">
       <NavigationLogo />
       <NavigationItemWrapper />
     </nav>
